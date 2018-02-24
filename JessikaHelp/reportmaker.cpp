aa6f7e5ce2471d98d76c7a24aa822c20b9be8e49
Alexander Antonov <saha_97@bk.ru>
2018-02-24 15:18:37 +0300

    Названия файлов по формату + фикс

--- JessikaHelp/reportmaker.cpp
+++ JessikaHelp/reportmaker.cpp
@@ -1,12 +1,22 @@
 #include "reportmaker.h"
 #include <QDebug>
+#include <QString>
 
 ReportMaker::ReportMaker(QObject *parent) : QObject(parent)
 {
 
 }
 
+//REWRITE!
 void ReportMaker::makeReportWordLabel(QList<QStringList> data){
+
+    // Поменять путь на нормальный!
+    //как отдельную функцию в "каком-нибудь классе"
+
+    QString newFile = "D:\\";
+    newFile += dateTime.currentDateTime().toString("yyyy-MM-dd");
+    newFile += "_" + dateTime.currentDateTime().toString("hh-mm");
+    newFile += "_label.doc";
 
     QString temporaryApplicationDirPath = QCoreApplication::applicationDirPath();;
     QString applicationDirPath;
@@ -16,14 +26,15 @@ void ReportMaker::makeReportWordLabel(QList<QStringList> data){
         else applicationDirPath+=temporaryApplicationDirPath[i];
     }
     applicationDirPath+="\\template_label.doc";
-    QFile::copy(applicationDirPath, "D:\\label.doc");
+    QFile::copy(applicationDirPath, newFile);
+
 
     QAxObject* mWord = new QAxObject( "Word.Application");
-    mWord->querySubObject("Documents")->querySubObject( "Open(const QString&)", "D:\\label.doc");
+    mWord->querySubObject("Documents")->querySubObject( "Open(const QString&)", newFile);
     mWord->querySubObject("Selection")->querySubObject("Start", 0);
     mWord->querySubObject("Selection")->querySubObject("End", 0);
 
-    int n = 5; // QTABLEWIDGET ROWS
+    int n = data.size(); // QTABLEWIDGET ROWS
      const int colsLabel = 4;
      const int rowsLabel = (n + colsLabel - 1) / colsLabel;
      const int pageWidth = 210 - 2;
@@ -43,37 +54,13 @@ void ReportMaker::makeReportWordLabel(QList<QStringList> data){
                               mWord->querySubObject("ActiveDocument")->dynamicCall("Range()"), rowsLabel, colsLabel, 1, 1);
 
      QAxObject* cell;
-     /*cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(1), QVariant(1)) ;
-     cell->setProperty("Width",widthCenter * kMM);
-     cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(1), QVariant(2)) ;
-     cell->setProperty("Width",widthCenter * kMM);
-     cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(1), QVariant(3)) ;
-     cell->setProperty("Width",widthCenter * kMM);
-     cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(1), QVariant(4)) ;
-     cell->setProperty("Width",widthCenter * kMM);
-     cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(2), QVariant(1)) ;
-     cell->setProperty("Width",widthCenter * kMM);
-     cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(2), QVariant(2)) ;
-     cell->setProperty("Width",widthCenter * kMM);
-     cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(2), QVariant(3)) ;
-     cell->setProperty("Width",widthCenter * kMM);
-     cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(2), QVariant(4)) ;
-     cell->setProperty("Width",widthCenter * kMM);*/
-/*
-     const double kMM = 110.0 / 38.8; // коэффициент для перевода мм в единицы ворда
-     for(int x = 1; x <= colsLabel; ++x) {
-         mWord->querySubObject("ActiveDocument")->querySubObject("Tables")
-                 ->querySubObject("Item(Table)", t)->querySubObject("Columns")
-                 ->querySubObject("Item(Column)", x)->querySubObject("Width",  widthCenter * kMM);
-       }
-*/
      QAxObject* boarder;
-      QString text;
+     QString text;
 
      const int dotStyle = 4;
 
      int tempN = 0;
-    for (int y = 1; y <=rowsLabel; y++)
+     for (int y = 1; y <=rowsLabel; y++)
      for(int x = 1; x <= colsLabel; ++x) {
          cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(y), QVariant(x)) ;
          cell->setProperty("Width",widthCenter * kMM);
@@ -85,9 +72,18 @@ void ReportMaker::makeReportWordLabel(QList<QStringList> data){
          boarder->setProperty("LineStyle",dotStyle);
          boarder = cell->querySubObject("Borders(xlEdge)", 4);
          boarder->setProperty("LineStyle",dotStyle);
+
          text = "";
-         if (tempN < n) text = data.at(tempN).at(0) +
-                 "\n" + data.at(tempN).at(1) +
+        /* if (tempN < n){
+            for(int i = 0; i < data.at(tempN).size(); i++){
+                QString s = data.at(tempN).at(i);
+                if (i == 1) s = "K-" + s;
+                QString enter = s.isEmpty() ? "" : "\n";
+                text = text + enter + s;
+            }
+         }*/
+         if (tempN < n) text = "\n" + data.at(tempN).at(0) +
+                 "\nK-" + data.at(tempN).at(1) +
                  "\n" + data.at(tempN).at(2) +
                  "\n" + data.at(tempN).at(3) +
                  "\n" + data.at(tempN).at(4) +
@@ -97,34 +93,8 @@ void ReportMaker::makeReportWordLabel(QList<QStringList> data){
          cell->querySubObject("Range")->querySubObject("Text", text);
      }
 
-
-   /* QAxObject* boarder;
-
-     for(int row = 1; row <= n; ++row) {
-         int x = ((row - 1) % colsLabel) + 1;
-         int y = ((row - 1) / colsLabel) + 1;
-         const int dotStyle = 4;
-
-
-         cell = newTable->querySubObject("Cell(Row, Column)" , QVariant(y), QVariant(x)) ;
-         boarder = cell->querySubObject("Borders(xlEdge)", 1);
-         boarder->setProperty("LineStyle",dotStyle);
-         boarder = cell->querySubObject("Borders(xlEdge)", 2);
-         boarder->setProperty("LineStyle",dotStyle);
-         boarder = cell->querySubObject("Borders(xlEdge)", 3);
-         boarder->setProperty("LineStyle",dotStyle);
-         boarder = cell->querySubObject("Borders(xlEdge)", 4);
-         boarder->setProperty("LineStyle",dotStyle);
-
-        //SetLineStyleBorderTable(t, y, x, true, true, true, true, dotStyle);
-        // SetTextToTable(t, y, x, GetInfo(row, false));
-       }
-*/
-
      mWord->querySubObject("Documents")->dynamicCall("Save()");
      mWord->querySubObject("Documents")->dynamicCall("Close()");
      mWord->dynamicCall("SetDisplayAlerts(bool)", FALSE);
      mWord->dynamicCall("Quit()");
-
-
 }
